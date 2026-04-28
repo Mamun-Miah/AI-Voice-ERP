834a021ed11f498f0edbbe7a0fab1a93582e91a3
Mamun-Miah <mahadyhasen7@gmail.com>
2026-04-28 11:59:50 +0600

    updated purchase return

--- src/purchases/purchases.module.ts
+++ src/purchases/purchases.module.ts
@@ -5,8 +5,8 @@ import { PurchaseReturnsService } from './purchase-returns.service';
 import { PurchaseReturnsController } from './purchase-returns.controller';
 
 @Module({
-  controllers: [PurchasesController, PurchaseReturnsController],
+  controllers: [PurchaseReturnsController, PurchasesController],
   providers: [PurchasesService, PurchaseReturnsService],
   exports: [PurchasesService, PurchaseReturnsService],
 })
-export class PurchasesModule {}
+export class PurchasesModule { }
